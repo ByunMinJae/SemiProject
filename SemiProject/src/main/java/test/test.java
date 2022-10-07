b12affbd63d9b21721635e5a4a90b97fcc49e565
Changmin <kcm2749@naver.com>
2022-10-07 12:43:13 +0900

    changmin - 221017 - test

---
+++ SemiProject/src/main/java/test/test.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test/test")
+public class test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("/test/test [GET]");
+		
+		req.getRequestDispatcher("/WEB-INF/views/test.jsp").forward(req, resp);
+	}
+	
+}
