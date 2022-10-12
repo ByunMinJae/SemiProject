e1f7bb6d95ab768b38ff56852b4bffc76a93e269
byunm <byunm@DESKTOP-PUKB97F>
2022-10-12 21:40:43 +0900

    minjae - 221012 - add class

---
+++ SemiProject/src/main/java/minjae/controller/FindUserIdController.java
@@ -0,0 +1,13 @@
+package minjae.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class FindUserIdController
+ */
+@WebServlet("/find/userid")
+public class FindUserIdController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
