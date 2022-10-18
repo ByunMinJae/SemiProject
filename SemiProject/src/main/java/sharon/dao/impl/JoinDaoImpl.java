adf778abc6dc7e405b017282fa23041015b3da27
sharon <sharon@DESKTOP-COGVNTE>
2022-10-18 22:05:23 +0900

    sharon -221018-merge

--- SemiProject/src/main/java/sharon/dao/impl/JoinDaoImpl.java
+++ SemiProject/src/main/java/sharon/dao/impl/JoinDaoImpl.java
@@ -1,11 +1,9 @@
 package sharon.dao.impl;
 
-import java.sql.Connection;
+import java.sql.Connection; 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import common.JDBCTemplate;
 import sharon.dao.face.JoinDao;
@@ -13,23 +11,28 @@ import sharon.dto.User;
 
 public class JoinDaoImpl implements JoinDao {
 	
-	private PreparedStatement ps =null;
-	private ResultSet rs=null;
+	private PreparedStatement ps;
+	private ResultSet rs;
 	
-	
-	public int selectNextUserno(Connection conn) {
-	
-		String sql = "SELECT cmc_seq.nextval FROM dual";
-	
-		int nextval = 0;
+	public int selectCntMemberByUseridUserpw(Connection conn, User user) {
+		
+		String sql = "";
+		sql += "SELECT count(*) cnt FROM user_info";
+		sql += " WHERE userid = ?";
+		sql += "	AND userpw = ?";
+		
+		int cnt = 0;
 		
 		try {
 			ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getUserid());
+			ps.setString(2, user.getUserpw());
+			
 			rs = ps.executeQuery();
 			
-			rs.next();
-			
-			nextval = rs.getInt(1);
+			while(rs.next()) {
+				cnt = rs.getInt("cnt");
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -37,49 +40,79 @@ public class JoinDaoImpl implements JoinDao {
 			JDBCTemplate.close(rs);
 			JDBCTemplate.close(ps);
 		}
-		
-		return nextval;
+				
+		return cnt;
 	}
+	
+	@Override
+	public User selectMemberByUserid(Connection conn, User user) {
 
+		String sql = "";
+		sql += "SELECT userid, userpw, username FROM user_info";
+		sql += " WHERE userid = ?";
+		
+		User result = null;
+		
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, user.getUserid());
+			
+			rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				result = new User();
+				
+				result.setUserid( rs.getString("userid") );
+				result.setUserpw( rs.getString("userpw") );
+//				result.setUsername( rs.getString("username") );
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(ps);
+		}
+				
+		return result;
+		
+	}
+	
+	@Override
 	public int insert(Connection conn, User user) {
 		
 		String sql = "";
-		sql += "INSERT INTO user ( userno, userid, ,userpw, gender,address,phone,birth, email, nick, joinday,userupdate,gradeno)";
-		sql += " VALUES( ?, ?, ?, ? ?,?,?,?,?,?,?,?)";
+		sql += "INSERT INTO user_info ( userid, userpw, username, nick, birth, gender, email, phone, address )";
+		sql += " VALUES ( ?, ?, ? ,? ,? ,? ,? ,? ,?)";
 		
+		int res = 0;
 		
-		
-		//INSERT 수행 결과 변수
-		int result = 0;
-				
 		try {
 			ps = conn.prepareStatement(sql);
-					
-			ps.setInt(1, user.getUserno());
-			ps.setString(2, user.getUserid());
-			ps.setString(3, user.getUserpw());
-			ps.setString(4, user.getGender());
-			ps.setString(5, user.getAddress());
-			ps.setInt(6, user.getPhone());
-			ps.setDate(7, user.getBirth());
-			ps.setString(8, user.getEmail());
-			ps.setString(9, user.getNick());
-			ps.setDate(10, user.getJoinday());
-			ps.setDate(11, user.getUserupdate());
-			ps.setInt(12, user.getGradeno());
-					
-			result = ps.executeUpdate();
-					
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				JDBCTemplate.close(ps);
-			}
-				
-			return result;
-			}		
-	
-	
+			
+			ps.setString(1, user.getUserid());
+			ps.setString(2, user.getUserpw());
+//			ps.setString(3, user.getUsername());
+			ps.setString(4, user.getNick());
+			ps.setDate(5, user.getBirth());
+			ps.setString(6, user.getGender());
+			ps.setString(7, user.getEmail());
+			ps.setInt(8, user.getPhone());
+			ps.setString(9, user.getAddress());
+//			ps.setDate(10, user.getJoinday());
+//			ps.setDate(11, user.getUserupdate());
+//			ps.setInt(12, user.getGradeno());
+			
+			res = ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(ps);
+		}
+		
+		return res;
+	}
 			/*
 			 * //10/14 ->회원목록 조회 ... public List<User> selectAll(Connection conn){
 			 * 
@@ -113,4 +146,16 @@ public class JoinDaoImpl implements JoinDao {
 			 * @Override public User selectByUserno(Connection conn, int userno) { // TODO
 			 * Auto-generated method stub return null; }
 			 */
+
+	@Override
+	public User selectMemberByUserpw(Connection connection, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int selectNextUserno(Connection conn) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
