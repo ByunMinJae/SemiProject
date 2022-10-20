6f8dbe07214ddf1bf860e20a67c7ec2c9050772c
Changmin <kcm2749@naver.com>
2022-10-21 03:42:57 +0900

    changmin - 221021 - feat: 폰트추가, 메인페이지변경

--- SemiProject/src/main/java/changmin/controller/MainController.java
+++ SemiProject/src/main/java/changmin/controller/MainController.java
@@ -17,6 +17,7 @@ public class MainController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("/main [GET]");
 	
+		
 		req.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(req, resp);
 		
 	}
